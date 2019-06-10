2670ba7f97d0f7569c653e78a877604391b2d8b8
itay1542 <47055981+itay1542@users.noreply.github.com>
2019-06-10 11:50:29 +0300

    Update Missile.js

--- Missile.js
+++ Missile.js
@@ -6,17 +6,21 @@ class Missile{
         this.height = height
         this.width = width
         this.color = color
+        this.velocity = 0
+        this.angle = 0
     }
 
-    //velocity in pixels per second
-
-    launch(angle,velocity){             
-        setInterval(function(){this.x =this.x*velocity*Math.cos(angle * Math.PI / 180)
-                              this.y =this.y*velocity*Math.sin(angle * Math.PI / 180)}, 1000);
-    }
+    //velocity in coordinates per second
 
     calcDistance(curX,curY,destX,destY){
         return(Math.sqrt(Math.abs(destX-curX)+Math.abs(destY-curY)))
+    }
+
+    launch(angle,velocity){
+        this.velocity = velocity
+        this.angle = angle
+        setInterval(function(){this.x = this.x*velocity*Math.cos(angle * Math.PI / 180)
+                               this.y = this.y*velocity*Math.sin(angle * Math.PI / 180)}, 1000);
     }
 
 }
